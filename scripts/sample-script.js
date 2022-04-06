bee5b359b76f76235dbcdd8522d9b837d9813398
Dendolium <dendodendolium@gmail.com>
2022-04-06 22:46:14 +0530

    simple greeter UI

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +15,28 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
+
+
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("deployer address: ", deployer.address)
+
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
+  console.log("contract address: ", greeter.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+
+  //data to be saved for frontend interactions
+  const data = {
+    address: greeter.address,
+    abi: JSON.parse(greeter.interface.format('json'))
+  };
+
+
+  //need to access abi hence saving it somewhere
+  fs.writeFileSync('src/compiled-json/Greeter.json', JSON.stringify(data));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
+import { useEffect, useRef, useState } from 'react';
+import { ethers } from "ethers";
+import artifact from "./compiled-json/Greeter.json";
+
 
 function App() {
+
+  //saving the contract in a ref
+  //can use useState too
+  //contract.current value won;t change on re-render once initialized
+  const contract = useRef();
+
+  //state variables
+  const [greetingValue, setGreetingValue] = useState("fetching")
+
+
+  //FUNCTIONS
+  //getting the greeting value
+  //cant use the set function directly, it';ll save the promise instead of the fetched value
+  const fetchGreetingValue = async () => {
+    const value = await contract.current.greet();
+    setGreetingValue(value);
+  }
+  
+  useEffect(() => {
+
+    //cant use asycn with useEffect, it causes a race condition
+    //instead making another async function 
+
+    const setup = async () => {
+      const provider = new ethers.providers.JsonRpcProvider();
+      const network = await provider.getNetwork();
+
+
+      contract.current = await new ethers.Contract(
+        artifact.address,
+        artifact.abi,
+        provider.getSigner(),
+      );
+      
+    
+      //this has to be inside this function
+      //caling it outside would make call the greet function on an unresolved promise
+      fetchGreetingValue();
+    }
+
+    setup();
+
+
+
+  }, [])
+
   return (
     <div className="App">
-      <h1>i still got it buabahhahahh</h1>
+      <h1>{greetingValue}</h1>
     </div>
   );
 }
