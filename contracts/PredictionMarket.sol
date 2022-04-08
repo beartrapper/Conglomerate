74db80de5bb0670133a258837fe617b9f621f67c
Dendolium <dendodendolium@gmail.com>
2022-04-08 20:13:56 +0530

    basic structure done

---
+++ contracts/PredictionMarket.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+
+contract PredictionMarket {
+    // DB
+    // array of all players indicating balances
+    // array of all players indicating history of players
+    
+    // FUNCTIONS
+    // deposit for bet
+    // get value for checking(chainlink)
+    // determine
+    // withdraw from contract(only owner)
+
+}
